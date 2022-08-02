152691f1f648f7212848e82d1bfebd81c18312af
ocampoad <ocampo.adriane13@gmail.com>
2022-08-01 23:07:06 -0700

    added category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -12,21 +12,43 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Products
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+  const dbCategoriesDatabyID = await Category.findByPk(req.params.id);
+  const categorybyID = dbCategoriesDatabyID.get({plain:true});
+  res.json(categorybyID);
   // find one category by its `id` value
   // be sure to include its associated Products
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  const newCategory = await Category.create({
+    category_name: req.body.category_name
+  });
+  const dbcategoriesData = await Category.findAll();
+  const categories = dbcategoriesData.map(dbCategories => dbCategories.get({plain: true}));
+  res.json(categories);
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id',async (req, res) => {
   // update a category by its `id` value
+  const dbCategoriesDatabyID = await Category.findByPk(req.params.id);
+  dbCategoriesDatabyID.update({
+    category_name: req.body.category_name
+  });
+  await dbCategoriesDatabyID.save()
+  const dbcategoriesData = await Category.findAll();
+  const categories = dbcategoriesData.map(dbCategories => dbCategories.get({plain: true}));
+  res.json(categories);
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+router.delete('/:id', async (req, res) => {
+  // delete a category by its `id` 
+  const dbCategoriesDatabyID = await Category.findByPk(req.params.id);
+  await dbCategoriesDatabyID.destroy();
+  const dbcategoriesData = await Category.findAll();
+  const categories = dbcategoriesData.map(dbCategories => dbCategories.get({plain: true}));
+  res.json(categories);
 });
 
 module.exports = router;
